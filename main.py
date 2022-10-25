91d256fbd1696af5000b2c2fa98026f8cee501b7
Nícholas Eras <48137930+Nicholasmais@users.noreply.github.com>
2022-10-24 22:09:42 -0300

    Início do jogo

---
+++ main.py
@@ -0,0 +1,98 @@
+#https://www.youtube.com/watch?v=a4NVQC_2S2U&t=64s&ab_channel=NaseemShah
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
+import random
+
+player1_x, player1_y = -1, 0
+player2_x, player2_y = 1, 0
+
+player_width = 5
+player_height = 10
+
+ball_x, ball_y = 0,0
+
+ball_xstep = 0.1
+ball_ystep = 0.1
+
+windowWidth = random.randint(100,1000)
+windowHeight = random.randint(100,1000)
+
+window_coordinates = 100
+
+def DesenhaBrasil(width, height, y):
+  global player1_x, player1_y, player2_x, player2_y, player_width, player_height, ball_xstep, ball_ystep, window_height, window_width, window_coordinates
+
+  glColor3f(.5, .5, .5)
+  glBegin(GL_QUADS)
+
+  glVertex2f(-1*width,-1*height + y)
+  glVertex2f(-1*width,1*height + y)
+  glVertex2f(1*width,1*height + y)
+  glVertex2f(1*width,-1*height + y)
+
+  glEnd()
+
+
+def Desenha():
+  global player1_x, player1_y, player2_x, player2_y, player_width, player_height, ball_xstep, ball_ystep, window_height, window_width, window_coordinates
+  glClear(GL_COLOR_BUFFER_BIT)
+
+  glViewport(0,0,window_width,window_height)
+  glMatrixMode(GL_MODELVIEW)
+  glLoadIdentity()
+  gluOrtho2D(-window_coordinates,window_coordinates,-window_coordinates,window_coordinates)
+  DesenhaBrasil(10, 10, player1_y)
+  DesenhaBrasil(10, 10, player2_y)
+  glutSwapBuffers()
+
+def Inicializa():
+  glClearColor(0,0,0,1)
+
+def Timer(value):
+  global player1_x, player1_y, player2_x, player2_y, player_width, player_height, ball_xstep, ball_ystep, window_height, window_width, window_coordinates
+  if abs(player1_x) > window_coordinates-1:
+    ball_xstep *= -1
+
+  if abs(player1_y) > window_coordinates-1:
+    ball_ystep *= -1
+
+  player1_x += ball_xstep
+  player1_y += 1.5*ball_ystep
+  glutPostRedisplay()
+  glutTimerFunc(33, Timer, 1)
+
+def Teclado(key, x, y):
+  global player1_x, player1_y, player2_x, player2_y, player_width, player_height, ball_xstep, ball_ystep, window_height, window_width, window_coordinates
+  match key:
+    case b"w":
+      y3 += abs(ball_ystep)
+    case b"a":
+      x3 -= abs(ball_xstep)
+    case b"s":
+      y3 -= abs(ball_ystep)
+    case b"d":
+      x3 += abs(ball_xstep)
+    
+  glutPostRedisplay()
+
+def Responsivo(width, height):
+  global player1_x, player1_y, player2_x, player2_y, player_width, player_height, ball_xstep, ball_ystep, window_height, window_width, window_coordinates
+  window_width = width
+  window_height = height
+
+def main():
+  glutInit(sys.argv)
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+  glutInitWindowPosition(0,0)
+  glutInitWindowSize(windowWidth,windowHeight)
+  glutCreateWindow(b"Brasil")
+  glutDisplayFunc(Desenha)
+  glutReshapeFunc(Responsivo)
+  glutKeyboardFunc(Teclado)
+  glutTimerFunc(33, Timer, 1)
+  Inicializa()
+  glutMainLoop()
+
+main()
